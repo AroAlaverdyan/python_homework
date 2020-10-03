e2ce4f252eda15edd00cf5a2dfaef30783227bf9
Aro Alaverdyan <alaverdyanara2000@gmail.com>
2020-10-03 23:11:05 +0400

    new homework

---
+++ repetition.py
@@ -0,0 +1,14 @@
+my_dict = {'1':['a','b'], '2':['c', 'd'], '3':['x', 'y']}
+
+new_list = []
+for i in my_dict.keys():
+	new_list += i
+
+circle_for_1 = 0
+
+for value_1 in my_dict[new_list[circle_for_1]]:
+	circle_for_1 += 1
+	for value_2 in my_dict[new_list[1]]:
+		for value_3 in my_dict[new_list[2]]:
+			print(value_1 + value_2 + value_3)
+		
