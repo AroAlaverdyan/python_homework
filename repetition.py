e2454d5a70bcb76e2cba90bbb733934d5808e25d
Aro Alaverdyan <alaverdyanara2000@gmail.com>
2020-10-09 22:36:01 +0400

    hogna

--- repetition.py
+++ repetition.py
@@ -12,8 +12,3 @@ for value_1 in my_dict[new_list[circle_for_1]]:
 		for value_3 in my_dict[new_list[2]]:
 			print(value_1 + value_2 + value_3)
 
-			
-for i in range(0,100)
-print("husov em exav, hly, hly")
-
-print("piti vor eli")
