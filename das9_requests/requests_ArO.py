603ec4a613afb13b72467254b9d32788b386374d
Aro Alaverdyan <alaverdyanara2000@gmail.com>
2020-10-27 23:34:27 +0400

    image(jpeg and png) downloader

--- das9_requests/requests_ArO.py
+++ das9_requests/requests_ArO.py
@@ -9,43 +9,46 @@ class ImageDownloader:
 			f.write(str(self.image_list))
 		return "text file has been created, which is contain list of jpeg and png image's links"
 
+link_list = ["http://httpbin.org/image/jpeg", "http://httpbin.org/image/png"]
 
-class ImageJPG(ImageDownloader):
-	def __init__(self, image_list):
-		super().__init__(image_list)
+for i in link_list:
+	if "jpeg" in i:
 
-	def download(self):
-		response = requests.get(self.image_list[0])
+		class ImageJPG(ImageDownloader):
+			def __init__(self, image_list):
+				super().__init__(image_list)
 
-		with open("image1.jpeg", "wb") as image_jpeg:
-			image_jpeg.write(response.content)
-		
-		return "JPEG image has been downloaded"
+			def download(self):
+				response = requests.get(i)
 
+				with open("image1.jpeg", "wb") as image_jpeg:
+					image_jpeg.write(response.content)
+				
+				return "JPEG image has been downloaded"
 
-class ImagePNG(ImageJPG):
-	def __init__(self, image_list):
-		ImageDownloader.__init__(self, image_list)
+	elif "png" in i:
+		class ImagePNG(ImageJPG):
+			def __init__(self, image_list):
+				ImageDownloader.__init__(self, image_list)
 
-	def download(self):
-		response = requests.get(self.image_list[1])
+			def download(self):
+				response = requests.get(i)
 
-		with open("image2.png", "wb") as image_png:
-			image_png.write(response.content)
+				with open("image2.png", "wb") as image_png:
+					image_png.write(response.content)
 
-		return "PNG image has been downloaded!!!"
+				return "PNG image has been downloaded!!!"
 
 class Result(ImagePNG):
 	def __init__(self, image_list):
 		ImageDownloader.__init__(self, image_list)
 
 	def result(self):
-		return f'{ImageJPG.download(self)},	\n{ImagePNG.download(self)}'
-
-link_list = ["http://httpbin.org/image/jpeg", "http://httpbin.org/image/png"]
+		print(ImageJPG.download(self))
+		print(ImagePNG.download(self))
 
 
 start = Result(link_list)
 
 print(start.txt_file())
-print(start.result())
+start.result()
