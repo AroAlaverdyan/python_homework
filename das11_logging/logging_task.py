a10218405a705c445a040d07602b7daf53e24448
Aro Alaverdyan <alaverdyanara2000@gmail.com>
2020-10-30 21:55:56 +0400

    logging_task

---
+++ das11_logging/logging_task.py
@@ -0,0 +1,46 @@
+import logging
+import random
+
+def num_dicider():
+
+	any_number = random.randint(0, 50)
+	print(any_number)
+	logging.basicConfig(filename = "number_errors.log", filemode = "w", format = "%(asctime)s -- %(levelname)s -- %(message)s", level = logging.DEBUG)
+
+	if any_number >= 0 and any_number <= 9:
+		logging.debug(f"Number is {any_number}")
+		logging.info("Program is normaly working")
+		logging.warning("The number is approximately to half of the max value.")
+		logging.error("The number is getting closer to the max value.")
+		logging.critical(f"Be carefull. Number({any_number}) is more closer to the max valu than before.")		
+				
+	elif any_number >= 10 and any_number <= 19:
+		logging.info("Program is normaly working")
+		logging.warning("The number is approximately to half of the max value.")
+		logging.error("The number is getting closer to the max value.")
+		logging.critical(f"Be carefull. Number({any_number}) is more closer to the max valu than before.")		
+
+	elif any_number >= 20 and any_number <= 29:
+		logging.warning("The number is approximately to half of the max value.")
+		logging.error("The number is getting closer to the max value.")
+		logging.critical(f"Be carefull. Number({any_number}) is more closer to the max valu than before.")
+		
+
+	elif any_number >= 30 and any_number <= 39:
+		logging.error("The number is getting closer to the max value.")
+		logging.critical(f"Be carefull. Number({any_number}) is more closer to the max valu than before.")
+
+	elif any_number >= 40 and any_number <= 50:
+		logging.critical(f"Be carefull. Number({any_number}) is more closer to the max valu than before.")
+
+
+for _ in range(3):
+	
+	num_dicider()
+		
+
+
+
+
+
+
--- das9_requests/requests_ArO.py
+++ das9_requests/requests_ArO.py
@@ -1,54 +1,96 @@
+# import requests
+
+# class ImageDownloader:
+# 	def __init__(self, image_list):
+# 		self.image_list = image_list
+
+# 	def txt_file(self):
+# 		with open("image_list.txt", "w") as f:
+# 			f.write(str(self.image_list))
+# 		return "text file has been created, which is contain list of jpeg and png image's links"
+
+# link_list = ["http://httpbin.org/image/jpeg", "http://httpbin.org/image/png"]
+
+
+# class ImageJPG(ImageDownloader):
+# 	def __init__(self, image_list):
+# 		super().__init__(image_list)
+
+# 	def download(self):
+# 		response = requests.get(i)
+
+# 		with open("image1.jpeg", "wb") as image_jpeg:
+# 			image_jpeg.write(response.content)
+				
+# 		return "JPEG image has been downloaded"
+
+
+# class ImagePNG(ImageJPG):
+# 	def __init__(self, image_list):
+# 		ImageDownloader.__init__(self, image_list)
+
+# 	def download(self):
+# 		response = requests.get(i)
+
+# 		with open("image2.png", "wb") as image_png:
+# 			image_png.write(response.content)
+
+# 		return "PNG image has been downloaded!!!"
+
+# class Result(ImagePNG):
+# 	def __init__(self, image_list):
+# 		ImageDownloader.__init__(self, image_list)
+
+# 	def result(self):
+# 		for i in link_list:
+# 			if "jpeg" in i:
+# 				print(ImageJPG.download(self))
+		
+# 			elif "png" in i:
+# 				print(ImagePNG.download(self))
+
+
+# start = Result(link_list)
+
+# print(start.txt_file())
+# start.result()
+# 	
+
+
+
 import requests
 
-class ImageDownloader:
-	def __init__(self, image_list):
-		self.image_list = image_list
+class Jpeg_image:
+    def download(self, link):
+        response = requests.get(link) 
+        with open("image1.jpeg", "wb") as image_png:     
+            image_png.write(response.content)
+        return "jpeg image has been downloaded!!!" 
 
-	def txt_file(self):
-		with open("image_list.txt", "w") as f:
-			f.write(str(self.image_list))
-		return "text file has been created, which is contain list of jpeg and png image's links"
+class Png_image:
+    
+    def download(self, link):
+        response = requests.get(link) 
+        with open("image2.png", "wb") as image_png:     
+            image_png.write(response.content)
+        return "PNG image has been downloaded!!!" 
 
+class ImageDownloader(Png_image,Jpeg_image):
+    def __init__(self, list_image):
+        self.list_image = list_image
+    
+    def download(self):
+        for link in self.list_image:
+            if link.endswith("png"):
+                text = Png_image.download(self, link)
+
+            elif link.endswith("jpeg"):
+                text = Jpeg_image.download(self, link)
+
+            else:
+                print("wrong type")
+            print(text)
+            
 link_list = ["http://httpbin.org/image/jpeg", "http://httpbin.org/image/png"]
-
-for i in link_list:
-	if "jpeg" in i:
-
-		class ImageJPG(ImageDownloader):
-			def __init__(self, image_list):
-				super().__init__(image_list)
-
-			def download(self):
-				response = requests.get(i)
-
-				with open("image1.jpeg", "wb") as image_jpeg:
-					image_jpeg.write(response.content)
-				
-				return "JPEG image has been downloaded"
-
-	elif "png" in i:
-		class ImagePNG(ImageJPG):
-			def __init__(self, image_list):
-				ImageDownloader.__init__(self, image_list)
-
-			def download(self):
-				response = requests.get(i)
-
-				with open("image2.png", "wb") as image_png:
-					image_png.write(response.content)
-
-				return "PNG image has been downloaded!!!"
-
-class Result(ImagePNG):
-	def __init__(self, image_list):
-		ImageDownloader.__init__(self, image_list)
-
-	def result(self):
-		print(ImageJPG.download(self))
-		print(ImagePNG.download(self))
-
-
-start = Result(link_list)
-
-print(start.txt_file())
-start.result()
+obj = ImageDownloader(link_list)
+obj.download()
